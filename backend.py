a8aa9f7e3d8f920a31cdb88f3010c8673cb890b3
Anik7303 <anik7303@gmail.com>
2020-12-22 23:04:04 +0600

    converted to object-oriented classes

--- backend.py
+++ backend.py
@@ -1,101 +1,97 @@
 import sqlite3
 
-def create_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect('database.db')
-    except Exception as e:
-        print(e)
-    return conn
-
-def execute_sql(sql_string, variables=[], single=False):
-    try:
-        conn = create_connection()
-        cur = conn.cursor()
-        cur.execute(sql_string, variables)
-        result = None
-        if single:
-            result = cur.fetchone()
-        else:
-            result = cur.fetchall()
-        conn.commit()
-        conn.close()
-        return result
-    except Exception as e:
-        print(e)
-
-def create_table():
-
-    sql_string = """
-    CREATE TABLE IF NOT EXISTS books(
-        _id INTEGER PRIMARY KEY,
-        title CHAR(100) NOT NULL,
-        author CHAR(40) NOT NULL,
-        year INTEGER NOT NULL,
-        isbn CHAR(30) NOT NULL
-    )
-    """
-    return execute_sql(sql_string)
-
-def drop_table():
-
-    sql_string = "DROP TABLE books"
-    return execute_sql(sql_string)
-
-def insert(title, author, year, isbn):
-
-    sql_string = """
-    INSERT INTO books (title, author, year, isbn)
-    VALUES (?, ?, ?, ?)
-    """
-    return execute_sql(sql_string, [title, author, year, isbn])
-
-def search(title = "", author = "", year = "", isbn = ""):
-
-    sql_string = """
-    SELECT * FROM books WHERE
-        title = ? OR
-        author = ? OR
-        year = ? OR
-        isbn = ?
-    """
-    return execute_sql(sql_string, [title, author, year, isbn])
-
-def view(id):
-
-    sql_string = "SELECT * FROM books WHERE _id = ?"
-    return execute_sql(sql_string, [id], True)
-
-def viewall():
+class Database(object):
+    def __init__(self, db_name):
+        sql_string = """
+        CREATE TABLE IF NOT EXISTS books(
+            _id INTEGER PRIMARY KEY,
+            title CHAR(100) NOT NULL,
+            author CHAR(40) NOT NULL,
+            year INTEGER NOT NULL,
+            isbn CHAR(30) NOT NULL
+        )
+        """
+        try:
+            self.conn = sqlite3.connect(db_name)
+            self.cur = self.conn.cursor()
+            self.cur.execute(sql_string)
+            self.conn.commit()
+        except Exception as e:
+            print(e)
     
-    sql_string = "SELECT * FROM books"
-    return execute_sql(sql_string)
+    def drop_table(self, table_name):
+        sql_string = "DROP TABLE ?"
+        try:
+            self.cur.execute(sql_string, [table_name])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
 
-def delete(id):
+    def insert(self, title, author, year, isbn):
+        sql_string = """
+        INSERT INTO books (title, author, year, isbn)
+        VALUES (?, ?, ?, ?)
+        """
+        try:
+            self.cur.execute(sql_string, [title, author, year, isbn])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
 
-    sql_string = "DELETE FROM books WHERE _id = ?"
-    return execute_sql(sql_string, [id])
+    def search(self, title = "", author = "", year = "", isbn = ""):
+        sql_string = """
+        SELECT * FROM books WHERE
+            title = ? OR
+            author = ? OR
+            year = ? OR
+            isbn = ?
+        """
+        try:
+            self.cur.execute(sql_string, [title, author, year, isbn])
+            return self.cur.fetchall()
+        except Exception as e:
+            print(e)
 
-def update(id, title = None, author = None, year = None, isbn = None):
+    def view(self, id):
+        sql_string = "SELECT * FROM books WHERE _id = ?"
+        try:
+            self.cur.execute(sql_string, [id], True)
+            return self.cur.fetchone()
+        except Exception as e:
+            print(e)
 
-    sql_string = """
-    UPDATE books
-    SET
-        title = ?,
-        author = ?,
-        year = ?,
-        isbn = ?
-    WHERE _id = ?
-    """
-    return execute_sql(sql_string, [title, author, year, isbn, id])
+    def viewall(self, ):
+        sql_string = "SELECT * FROM books"
+        try:
+            self.cur.execute(sql_string)
+            return self.cur.fetchall()
+        except Exception as e:
+            print(e)
 
-if __name__ == '__main__':
-    # conn = create_connection()
-    # create_table()
-    # insert('title 4', 'author 2', 1999, '2344623')
-    # delete(5)
-    # print(view(4))
-    # update(3, author="author 2", year=2003)
-    print(viewall())
-    # drop_table()
-    # conn.close()
+    def delete(self, id):
+        sql_string = "DELETE FROM books WHERE _id = ?"
+        try:
+            self.cur.execute(sql_string, [id])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    def update(self, id, title = None, author = None, year = None, isbn = None):
+        sql_string = """
+        UPDATE books
+        SET
+            title = ?,
+            author = ?,
+            year = ?,
+            isbn = ?
+        WHERE _id = ?
+        """
+        try:
+            self.cur.execute(sql_string, [title, author, year, isbn, id])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        self.conn.close()
+
